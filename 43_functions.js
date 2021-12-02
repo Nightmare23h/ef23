10ffee228a15687eee39e17c34de68e0b71c2cb7
Nightmare23h <64424102+Nightmare23h@users.noreply.github.com>
2021-12-02 08:46:55 +0100

    Finished Exercises

--- 43_functions.js
+++ 43_functions.js
@@ -21,8 +21,7 @@ const oneLesson = getSeconds(0, 45, 0) // should return 2700
 
 //Create HTML heading tag
 function createHeading(text) {
-	//@TODO
-	return "<h1>"+text+"</h1>"
+	return "<h1>"+text+"</h1>";
 }
 const heading = createHeading("Welcome") // should return "<h1>Welcome</h1>"
 
@@ -30,8 +29,8 @@ const heading = createHeading("Welcome") // should return "<h1>Welcome</h1>"
 
 //Create SVG rect
 function createSvgRect(x, y, w, h) {
-	//@TODO
-	
+
+	return `<rect x='${x}' y='${y}' width='${w}' height='${h}'></rect>`
 
 }
 const svgRect = createSvgRect(100, 200, 300, 400) // should return "<rect x='100' y='200' width='300' height='400'></rect>"
@@ -40,8 +39,23 @@ const svgRect = createSvgRect(100, 200, 300, 400) // should return "<rect x='100
 
 // Find median of 3 values
 function medianOf3(a, b, c) {
-	//@TODO
-	return 
+	if(a > b) {
+		if(b > c) {
+			return b;
+		} else if (a > c) {
+			return c;
+		} else {
+			return a;
+		}
+	} else {
+		if(a > c) {
+			return a;
+		} else if (b > c) {
+			return c;
+		} else {
+			return b;
+		}
+	}
 }
 const m = medianOf3(8, 11, 9) // should return 9
 
@@ -49,8 +63,7 @@ const m = medianOf3(8, 11, 9) // should return 9
 
 // Find average of 3 values
 function averageOf3(a, b, c) {
-	//@TODO
-	return 
+	return (a+b+c)/3;
 }
 const a = averageOf3(4, 11, 9) // should return 8
 
@@ -58,8 +71,9 @@ const a = averageOf3(4, 11, 9) // should return 8
 
 // Find average of array
 function getAverage(array) {
-	//@TODO
-	return 
+	var res = 0;
+	array.forEach(x => res+=x);
+	return res / array.length;
 }
 const b = getAverage([4, 11, 9, 2, 9]) // should return 7
 
@@ -67,8 +81,18 @@ const b = getAverage([4, 11, 9, 2, 9]) // should return 7
 
 // Solve ax^2 + bx + c = 0
 function solveQuadratic(a, b, c) {
-	//@TODO
-	return 
+
+	var D = (b*b - 4 * a * c);
+	if (D > 0){
+		return [
+			(-b - D ** 0.5) / (2 * a),
+			(-b + D ** 0.5) / (2 * a),
+		]
+	} else if (D == 0) {
+		return [-b / (2 * a)];
+	} else {
+		return [];
+	}
 }
 const solutions = solveQuadratic(1, 0, -16) // should return [-4, 4]
 const solution = solveQuadratic(1, -2, 1) // should return [1]
